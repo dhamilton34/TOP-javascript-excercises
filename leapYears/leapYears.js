89a12a1ee09694e68ae88bf48efb34589db3173a
Donte Hamilton <dontehamiltondesigns@gmail.com>
2021-04-11 15:09:20 -0400

    completed leapYears, but needed help

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,31 @@
-const leapYears = function() {
+const leapYears = function(years) {
 
+    let results;
+
+   /* if(years%4 == 0) {
+
+        results = true;
+        
+        if(years%100 == 0 && years%400 == 0)
+        {
+            results = true;
+        } else {
+            results = false;
+        }
+        
+    } else {
+        results = false;
+    }*/
+
+    if(years%4 === 0 && (years%100 !== 0 || years%400 == 0)) {
+        results = true;
+    } else {
+        results = false;
+    }
+
+    return results;
 }
+   
+
 
 module.exports = leapYears
