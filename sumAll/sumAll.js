15687939f0f97942772d1da72089c1bf9b05e096
Donte Hamilton <dontehamiltondesigns@gmail.com>
2021-04-11 13:52:34 -0400

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let startNum = 0;
+    let endNum = 0;
+    let finalNumber = 0;
 
+    if(typeof(x) != 'number' || typeof(y) != 'number'){return "ERROR"};
+
+    if(x < 0 || y < 0){return "ERROR"};
+    
+   /* if(x < y) {
+        startNum = x;
+        endNum = y;
+    } else {
+        startNum = y;
+        endNum = x;
+    }*/
+
+    //ternary operator
+    startNum = (x < y) ? x:y;
+    endNum = (y > x) ? y:x;
+    
+    for (startNum; startNum <= endNum; startNum++ ) {
+        
+        finalNumber += startNum;
+        //console.log(finalNumber);
+    }
+
+    return finalNumber;
 }
 
 module.exports = sumAll
