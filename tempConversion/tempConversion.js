9d6ea6cb83afe874fb8569f3bc2bc0e2413bfda9
Donte Hamilton <dontehamiltondesigns@gmail.com>
2021-04-11 15:26:29 -0400

    completed teampConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  let celsius = Math.round(((deg - 32)*(5/9))*10)/10;
+  console.log(celsius)
+  return celsius;
 }
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  let fahrenheit = Math.round(((deg*(9/5)+32))*10)/10;
+  console.log(fahrenheit);
+  return fahrenheit;
 }
 
 module.exports = {
