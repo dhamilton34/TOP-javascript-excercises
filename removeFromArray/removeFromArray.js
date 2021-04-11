695a2827039e15ead50cf0b02c9bd6150cb2d097
Donte Hamilton <dontehamiltondesigns@gmail.com>
2021-04-11 12:24:27 -0400

    Completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
 const removeFromArray = function() {
 
+    let args = [...arguments];
+    let input = args.shift(); 
+
+    for (let i=0; i < args.length; i++ ) {
+
+        let rmxLocation = input.indexOf(args[i]);
+        console.log(rmxLocation);
+
+        if(rmxLocation < 0) {
+            continue;
+        } 
+        
+        input.splice(rmxLocation, 1);
+        console.log(input);
+       
+       
+    }
+    
+    return input;
+    console.log(input);
 }
 
 module.exports = removeFromArray
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,10 +1,12 @@
 const reverseString = function(word) {
 
-    const revWord = Array.from(word).reverse();
+    /*const revWord = Array.from(word).reverse();
 
     const reversed = revWord.join("");
     
-    return reversed;
+    return reversed;*/
+
+    return word.split('').reverse().join('');
 }
 
 module.exports = reverseString
