081719c06d02d7be3e693fa7f98dc02190662a18
Donte Hamilton <dontehamiltondesigns@gmail.com>
2021-04-11 10:47:52 -0400

    Completed helloWorld, repeatString, and reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    const revWord = Array.from(word).reverse();
+
+    const reversed = revWord.join("");
+    
+    return reversed;
 }
 
 module.exports = reverseString
